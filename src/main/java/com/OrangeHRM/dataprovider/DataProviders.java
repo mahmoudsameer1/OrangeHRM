511e9f53c787325466351287750beaf948dd562a
mounayousef100 <118472640+mounayousef100@users.noreply.github.com>
2023-01-02 19:31:00 +0000

    updet class dataprovide

--- src/main/java/com/OrangeHRM/dataprovider/DataProviders.java
+++ src/main/java/com/OrangeHRM/dataprovider/DataProviders.java
@@ -3,10 +3,9 @@
  */
 package com.OrangeHRM.dataprovider;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+
+
 
 import org.testng.annotations.DataProvider;
 
@@ -31,4 +30,36 @@ public class DataProviders {
 		}
 		return data;
 	}
+	
+	@DataProvider(name = "AddNewReportsPage")
+	public Object[][]  AddNewReportsPage() {
+		int rows = obj.getRowCount("AddNewReportsPage");
+		int column = obj.getColumnCount("AddNewReportsPage");
+		int actRows = rows - 1;
+		Object[][] data = new Object[actRows][column];
+
+		for (int i = 0; i < actRows; i++) {
+			for (int j = 0; j < column; j++) {
+				data[i][j] = obj.getCellData("AddNewReportsPage", j, i + 2);
+			}
+		}
+		return data;
+	}
+	
+	@DataProvider(name = "configurationOptions_AddCustom")
+	public Object[][] configurationOptions_AddCustom() {
+		int rows = obj.getRowCount(" configurationOptions_AddCustom");
+		int column = obj.getColumnCount(" configurationOptions_AddCustom");
+		int actRows = rows - 1;
+		Object[][] data = new Object[actRows][column];
+
+		for (int i = 0; i < actRows; i++) {
+			for (int j = 0; j < column; j++) {
+				data[i][j] = obj.getCellData("configurationOptions_AddCustom", j, i + 2);
+			}
+		}
+		return data;
+	}
+	
+	
 }
