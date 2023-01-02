9274c2868be2eb7a0bdc9ba476de46cb42c74f50
mounayousef100 <118472640+mounayousef100@users.noreply.github.com>
2023-01-02 19:28:02 +0000

    add new class report test

--- src/test/java/com/OrangeHRM/PIMTests/ReportsTest.java
+++ src/test/java/com/OrangeHRM/PIMTests/ReportsTest.java
@@ -4,11 +4,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import com.OrangeHRM.Base.Base;
+import com.OrangeHRM.PIMPageObject.ReportsPage;
+import com.OrangeHRM.PageObject.DashboardPage;
+import com.OrangeHRM.PageObject.LoginPage;
 
 public class ReportsTest extends Base {
-
+	  private ReportsPage reportsPage ;
+	  private LoginPage loginPage; ;  
+	  private DashboardPage dashboardPage;
+	
 	@Parameters("browser")
 	@BeforeMethod
 	public void setup(String browser) {
@@ -17,12 +22,21 @@ public class ReportsTest extends Base {
 
 	@AfterMethod
 	public void tearDown() {
-		getDriver().quit();
+		//getDriver().quit();
 	}
 
 	@Test
-	public void loginTest() {
-
+	public void Search_for_Report() throws InterruptedException {
+		 reportsPage = new ReportsPage();
+	      loginPage  = new LoginPage();
+	      dashboardPage = new DashboardPage();
+	      reportsPage = new ReportsPage();
+	      dashboardPage = loginPage.login(prop.getProperty("username"),prop.getProperty("password"));
+	      dashboardPage.clickOnlistPageBtn();
+	      reportsPage = dashboardPage.clickOnReportsPageBtn();
+	      reportsPage.Search_the_report();
+	      reportsPage. DeleteReport();
+	    
 	}
 
 }
