db66ac8f709e608e7e2f66aaa2a4e7131dfa4cb4
mounayousef100 <118472640+mounayousef100@users.noreply.github.com>
2023-01-03 09:14:20 +0000

    dashboard

--- src/main/java/com/OrangeHRM/PageObject/DashboardPage.java
+++ src/main/java/com/OrangeHRM/PageObject/DashboardPage.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.OrangeHRM.Base.Base;
+import com.OrangeHRM.PIMPageObject.Conf_AddReportingMethods;
+import com.OrangeHRM.PIMPageObject.Conf_AddTerminationReasons;
+import com.OrangeHRM.PIMPageObject.ConfigurationOptionsPage;
 import com.OrangeHRM.PIMPageObject.EmployeeListPage;
+import com.OrangeHRM.PIMPageObject.ReportsPage;
+import com.OrangeHRM.PIMPageObject.configurationOptions_AddCustom;
 import com.OrangeHRM.actiondriver.Action;
 
 public class DashboardPage extends Base {
@@ -17,6 +22,15 @@ public class DashboardPage extends Base {
 
 	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")
 	private WebElement PIMPageBtn;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")
+	private WebElement listPageBtn;
+
+	@FindBy( xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[4]")
+	private WebElement ReportsPageBtn;
+	
+	@FindBy( xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span/i")
+	private WebElement ClickConfiguration;
 	
 	public DashboardPage() {
 		PageFactory.initElements(getDriver(), this);
@@ -32,4 +46,41 @@ public class DashboardPage extends Base {
 		action.click(getDriver(), PIMPageBtn);
 		return new EmployeeListPage();
 	}
+	
+    public EmployeeListPage clickOnlistPageBtn() {
+		action.implicitWait(getDriver(), 5000);
+		action.click(getDriver(),listPageBtn);
+		return new EmployeeListPage();
+	}
+   public ReportsPage clickOnReportsPageBtn() {
+		action.implicitWait(getDriver(), 5000);
+		action.click(getDriver(),ReportsPageBtn);
+     	return new ReportsPage();
+	  }
+    public ConfigurationOptionsPage clickConfiguration() {
+		action.implicitWait(getDriver(), 5000);
+		action.click(getDriver(), ClickConfiguration);
+    	return new  ConfigurationOptionsPage ();
+	  }
+    public configurationOptions_AddCustom clickcoCustomFieldsPage() {
+  		action.implicitWait(getDriver(), 5000);
+  		action.click(getDriver(), ClickConfiguration);
+      	return new  configurationOptions_AddCustom();
+  	  }
+    
+    public Conf_AddReportingMethods clickcofAddReportingMethods() {
+  		action.implicitWait(getDriver(), 5000);
+  		action.click(getDriver(), ClickConfiguration);
+      	return new  Conf_AddReportingMethods();
+  	  }
+    
+    public Conf_AddTerminationReasons clickCoAddTerminationReasons() {
+  		action.implicitWait(getDriver(), 5000);
+  		action.click(getDriver(), ClickConfiguration);
+      	return new Conf_AddTerminationReasons();
+  	  }
+   
+   
+   
+   
 }
